d49fa4155cd1a768960180febe9f67522ca6b60d
RupeshPatil <rupesh_patil@epam.com>
2026-06-26 15:10:35 -0700

    Spring AI dependencies and config

---
+++ SpringEcomBackend/src/main/java/com/practice/spring_ai/config/AiChatClientConfig.java
@@ -0,0 +1,14 @@
+package com.practice.spring_ai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiChatClientConfig {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
+}
