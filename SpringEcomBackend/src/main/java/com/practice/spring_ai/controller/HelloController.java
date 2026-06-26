3dd19d4f1c7eac884489cf3537492c1c885cd554
RupeshPatil <rupesh_patil@epam.com>
2026-06-25 18:30:39 -0700

    Ecomm UI and Backend

---
+++ SpringEcomBackend/src/main/java/com/practice/spring_ai/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.practice.spring_ai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greet(){
+        return "Welcome to Home Controller";
+    }
+
+}
