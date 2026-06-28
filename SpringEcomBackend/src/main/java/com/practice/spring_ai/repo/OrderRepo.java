6ae485846daac41ed49538464b60be6295d31261
RupeshPatil <rupesh_patil@epam.com>
2026-06-28 08:20:32 -0700

    Order service, repo and controller

---
+++ SpringEcomBackend/src/main/java/com/practice/spring_ai/repo/OrderRepo.java
@@ -0,0 +1,10 @@
+package com.practice.spring_ai.repo;
+
+import com.practice.spring_ai.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OrderRepo extends JpaRepository<Order, Integer> {
+    Optional<Order> findByOrderId(String orderId);
+}
